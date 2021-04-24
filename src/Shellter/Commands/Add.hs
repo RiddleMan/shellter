258310a1fb245a0ce9b298db6f238bc9a72b6467
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-04-24 22:39:15 +0200

    Add finding of the project root

--- src/Shellter/Commands/Add.hs
+++ src/Shellter/Commands/Add.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Shellter.Commands.Add where
 
 import Shellter.Types
+import System.Directory
+
+configFilePath :: String
+configFilePath = "~/.shellter_history"
+
+hasGitFolder :: FilePath -> IO Bool
+hasGitFolder path = doesPathExist $ path ++ "/.git"
+
+findProjectRoot' :: String -> IO (Maybe String)
+findProjectRoot' "/" = return Nothing
+findProjectRoot' currPath =
+  hasGitFolder currPath >>=
+  (\case
+     True -> return (Just currPath)
+     False -> makeAbsolute (currPath ++ "../") >>= findProjectRoot')
+
+findProjectRoot :: String -> IO (Maybe String)
+findProjectRoot = findProjectRoot'
 
 run :: String -> String -> IO ()
 run path cmd = print ("path: " ++ path) >> print ("cmd: " ++ cmd)
