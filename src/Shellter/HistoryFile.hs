2f12d35cbff9b21e4fce9348270c93f9cadfce46
Artur Ptaszek <artur.ptaszek@gmail.com>
2021-04-26 18:59:29 +0200

    Add parsing functions

--- src/Shellter/HistoryFile.hs
+++ src/Shellter/HistoryFile.hs
@@ -2,6 +2,7 @@
 
 module Shellter.HistoryFile (saveEntry) where
 
+import Data.List.Split
 import Data.Time
 import System.Directory
 import System.Posix.Files
@@ -14,7 +15,17 @@ data HistoryEntry = HistoryEntry
     lastUsed :: String
   }
 
-instance Show HistoryEntry where show entry = path entry ++ ":" ++ projectPath entry ++ ":" ++ cmd entry ++ ":" ++ show (hits entry) ++ ":" ++ lastUsed entry
+instance Show HistoryEntry where
+  show entry =
+    path entry
+      ++ ":"
+      ++ projectPath entry
+      ++ ":"
+      ++ cmd entry
+      ++ ":"
+      ++ show (hits entry)
+      ++ ":"
+      ++ lastUsed entry
 
 getConfigFilePath :: IO FilePath
 getConfigFilePath = (++ "/.shellter_history") <$> getHomeDirectory
@@ -30,6 +41,29 @@ createIfDoesNotExist =
             True -> pure ()
             False -> getConfigFilePath >>= (`writeFile` "")
         )
+
+readHistoryFile :: IO [String]
+readHistoryFile =
+  lines
+    <$> ( getConfigFilePath
+            >>= readFile
+        )
+
+-- TODO: Rewrite to the instance of the Read typeclass
+parseHistoryLine :: String -> HistoryEntry
+parseHistoryLine =
+  ( \[path, projectPath, cmd, hits, lastUsed] ->
+      HistoryEntry
+        path
+        projectPath
+        cmd
+        (read hits :: Int)
+        lastUsed
+  )
+    . splitOn ":"
+
+parseHistoryEntries :: [String] -> [HistoryEntry]
+parseHistoryEntries = map parseHistoryLine
 
 saveEntry :: String -> String -> String -> IO ()
 saveEntry path projectPath cmd =
